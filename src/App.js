de5b7c08e0f27ddebefa22b41e3adf48bb5af324
’wujiahao <yzyw147@163.com>
2021-05-23 14:32:46 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
@@ -21,5 +22,51 @@ function App() {
     </div>
   );
 }
+class Test extends Component {
+  constructor() {
+    super();
+    this.state = {
+      arr: [1, 2, 3, 4, 5],
+      title: '这是标签',
+    }
+  }
+  tabClick(e) {
+    const el = e.nativeEvent.target;
+    this.setState({title: `这是标题${el.id}`});
+  }
+  render() {
+    const { arr, title } = this.state;
+    const { container, myClass } = style;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <ul onClick={this.tabClick.bind(this)} style={container}>
+        {
+          arr.map((item, index) => {
+            return (
+              <li id={index} key={index} style={myClass}>{item}</li>
+            )
+          })
+        }
+      </ul>
+      </div>
+    )
+  }
+}
 
-export default App;
+const style = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '200px',
+  },
+  myClass: {
+    listStyle: 'none',
+    flex: 1,
+    textAlign: 'center',
+    border: '1px solid #000',
+  }
+}
+
+export default Test;
